240cf66b76dcf8728b6b55c2159205891b82b88e
Sebastien <sebdrg@gmail.com>
2014-02-13 14:51:44 +0100

    Mise à jour de server.js

--- server.js
+++ server.js
@@ -13,22 +13,22 @@ app.configure(function () {
 	app.use(express.bodyParser({limit: '2000mb'}));
 
 	app.use(app.router);
-	app.use(express.static(__dirname + '/app'));
-	app.use("/bower_components", express.static(__dirname + '/bower_components'));
-	app.use("/test", express.static(__dirname + '/test'));
+	app.use('/panoply', express.static(__dirname + '/app'));
+	//app.use("/panoply/bower_components", express.static(__dirname + '/bower_components'));
+	//app.use("/test", express.static(__dirname + '/test'));
 	app.use(express.errorHandler({dumpExceptions: true, showStack: true, showMessage: true}));
 });
 
 app.get('/panoply', function(req,res){
  res.sendfile(__dirname + '/app/index.html');
 }); 
-app.post('/upload', presentationProvider.upload);
-app.post('/createPresentation', presentationProvider.createPresentation);
-app.get('/app/uploads/:id/:file', presentationProvider.getImage);
-app.post('/json', presentationProvider.json)
-app.post('/remove', presentationProvider.remove)
-app.get('/zip', presentationProvider.zip)
-app.post('/reinitialization', presentationProvider.reinitialization)
+app.post('/panoply/upload', presentationProvider.upload);
+app.post('/panoply/createPresentation', presentationProvider.createPresentation);
+app.get('/panoply/app/uploads/:id/:file', presentationProvider.getImage);
+app.post('/panoply/json', presentationProvider.json)
+app.post('/panoply/remove', presentationProvider.remove)
+app.get('/panoply/zip', presentationProvider.zip)
+app.post('/panoply/reinitialization', presentationProvider.reinitialization)
 
 
 var port = process.argv[2] || 5000;	
